69323b37da9b05c1faf8e4a2fd88acce2739ea09
janek-gross <50371707+janek-gross@users.noreply.github.com>
2020-08-17 11:26:10 +0200

    Add files via upload

---
+++ model.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+from config import *
+
+
+
+def discriminator(x):
+    splits = tf.keras.layers.Lambda(lambda x: tf.unstack(x, axis=1))(x)
+    state_0 = tf.keras.layers.Dense(Config.n_hidden_units_d)(splits[0])    
+    state_0 = tf.keras.layers.Activation(lambda x: tf.keras.activations.relu(x, alpha=0.1))(state_0)
+    state_0 = tf.keras.layers.Dense(Config.n_hidden_units_d)(state_0)
+    state_1 = tf.keras.layers.Dense(Config.n_hidden_units_d)(splits[0])
+    state_1 = tf.keras.layers.Activation(lambda x: tf.keras.activations.relu(x, alpha=0.1))(state_1)
+    state_1 = tf.keras.layers.Dense(Config.n_hidden_units_d)(state_1)
+    x = tf.compat.v1.keras.layers.CuDNNLSTM(Config.n_hidden_units_d, return_sequences=True)(x, initial_state = [state_0,state_1])
+    x = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(1, activation = "sigmoid"))(x)
+    return x
+
+def generator(x):
+    splits = tf.keras.layers.Lambda(lambda x: tf.unstack(x, axis=1))(x)
+    state_0 = tf.keras.layers.Dense(Config.n_hidden_units_d)(splits[0])
+    state_0 = tf.keras.layers.Activation(lambda x: tf.keras.activations.relu(x, alpha=0.1))(state_0)
+    state_0 = tf.keras.layers.Dense(Config.n_hidden_units_d)(state_0)
+    state_1 = tf.keras.layers.Dense(Config.n_hidden_units_d)(splits[0])
+    state_1 = tf.keras.layers.Activation(lambda x: tf.keras.activations.relu(x, alpha=0.1))(state_1)
+    state_1 = tf.keras.layers.Dense(Config.n_hidden_units_d)(state_1)
+    x = tf.compat.v1.keras.layers.CuDNNLSTM(Config.n_hidden_units_g, return_sequences=True)(x, initial_state = [state_0,state_1])
+    x = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(1))(x)
+    return x
+
---
+++ utils.py
@@ -0,0 +1,35 @@
+import numpy as np
+from config import *
+
+def create_sine_dataset(n_samples = 100000, n_steps = Config.n_steps):
+    x = np.linspace(0,2*np.pi, n_steps)
+    x = np.tile(x, (n_samples, 1))
+    a = np.random.rand(n_samples)
+    b = 2 * np.random.rand(n_samples)-1
+    c = np.random.rand(n_samples)
+    d = np.pi * (2 * np.random.rand(n_samples) - 1)
+    x = a[:,None] * np.sin(x/(c[:,None]+ 0.2) + d[:,None])+b[:,None]
+    return x
+
+def categorical_noise():
+    """
+    returns a categorical randomvector z = (z_c,z_n) where
+    z_c is a one-hot vector for the category and z_n is an n-dimensional normaldistributed.
+    z.shape = (batch_size, n_steps, n_clusters + dim_clusters)
+    """
+    
+    z_c = np.zeros((Config.batch_size, Config.n_steps, Config.n_clusters)).astype("float32")
+    c = np.random.choice(Config.n_clusters, size = (Config.batch_size, Config.n_steps), replace = True)
+    x,y = np.ogrid[0:Config.batch_size,0:Config.n_steps]
+    z_c[x,y,c] = 1
+    z_n = np.random.normal(0, Config.sigma_clusters, (Config.batch_size, Config.n_steps, Config.dim_clusters)).astype("float32")
+    norm = np.sum(np.abs(z_n)**2,axis=2)**(1./2)
+    norm = norm + 1e-10
+    z_n = (z_n/norm[:,:,None])
+    
+    return z_c,z_n
+
+def make_trainable(net, val):
+    net.trainable = val
+    for l in net.layers:
+        l.trainable = val
